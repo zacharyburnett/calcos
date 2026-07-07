faa6ec15c2fb0fb189051e21e3bb282672cfee69
zacharyburnett <zburnett@stsci.edu>
2026-07-07 13:01:03 -0400

    conditional on GIL

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 from setuptools import setup, Extension
 from numpy import get_include as numpy_includes
 from pathlib import Path
+import sysconfig
+
+
+FREE_THREADED_PYTHON = sysconfig.get_config_var("Py_GIL_DISABLED") == 1
 
 
 def c_sources(parent: str) -> list[str]:
@@ -23,10 +27,13 @@ def c_includes(parent: str, depth: int = 1):
 PACKAGENAME = "calcos"
 SOURCES = c_sources("src")
 INCLUDES = c_includes("src") + [numpy_includes()]
-MACROS = [
-    ("Py_LIMITED_API", 0x030B0000),  # PY_VERSION_HEX for 3.11
-]
+MACROS = []
+if not FREE_THREADED_PYTHON:
+    MACROS.append(("Py_LIMITED_API", 0x030B0000))  # PY_VERSION_HEX for 3.11
 
+SETUPTOOLS_OPTIONS = {}
+if not FREE_THREADED_PYTHON:
+    SETUPTOOLS_OPTIONS["bdist_wheel"] = {"py_limited_api": "cp311"}
 
 setup(
     ext_modules=[
@@ -35,8 +42,8 @@ setup(
             sources=SOURCES,
             include_dirs=INCLUDES,
             define_macros=MACROS,
-            py_limited_api=True,
+            py_limited_api=not FREE_THREADED_PYTHON,
         ),
     ],
-    options={'bdist_wheel': {'py_limited_api': 'cp311'}},
+    options=SETUPTOOLS_OPTIONS,
 )
